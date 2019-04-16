4c46c41294632e4b459887c46be2f76eb79b378d
EDZ <65422049@qq.com>
2019-04-16 11:10:31 +0800

    计数器工具CountDownLatch：能够使一个线程等待其他线程完成各自的工作后再执行

---
+++ my-concurrent/src/main/java/com/zzs/CountryEnum.java
@@ -0,0 +1,45 @@
+package com.zzs;
+
+public enum  CountryEnum {
+    ONE(1,"齐"),
+    TOW(2,"楚"),
+    THREE(3,"燕"),
+    FOUR(4,"赵"),
+    FIVE(5,"韩"),
+    SIX(6,"魏");
+
+    private Integer recode;
+    private String retMessage;
+
+    public static CountryEnum forEach_CountryEnum(int index)
+    {
+        CountryEnum[] myArray = CountryEnum.values();
+        for (CountryEnum element : myArray) {
+            if (index == element.recode) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+    CountryEnum(Integer recode, String retMessage) {
+        this.recode = recode;
+        this.retMessage = retMessage;
+    }
+
+    public Integer getRecode() {
+        return recode;
+    }
+
+    public void setRecode(Integer recode) {
+        this.recode = recode;
+    }
+
+    public String getRetMessage() {
+        return retMessage;
+    }
+
+    public void setRetMessage(String retMessage) {
+        this.retMessage = retMessage;
+    }
+}
