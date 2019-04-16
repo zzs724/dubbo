03dcf3f39cfdfb46a12206cf0e0e4674f34c8ef5
EDZ <65422049@qq.com>
2019-04-16 15:52:51 +0800

    阻塞队列

--- my-concurrent/src/main/java/com/zzs/BlockingQueueDemo.java
+++ my-concurrent/src/main/java/com/zzs/BlockingQueueDemo.java
@@ -6,7 +6,20 @@ package com.zzs;
  * SynchronousQueue：不存储元素的阻塞队列，每个插入操作必须等到另一个线程调用移除操作，否则插入一直处于阻塞，
  *                  吞吐量高于LinkedBlockingQueue
  *
+ * 阻塞队列：当队列为空时，获取队列元素时线程会被阻塞
+ *            当队里满时，添加元素会被阻塞
+ * 阻塞队列会管理线程何时需阻塞，何时需被唤醒
  *
  */
-public class BlockingQueueDemo {
+public class BlockingQueueDemo
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+
+
 }
