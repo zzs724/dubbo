8bb3260240e372e1c524299fd620f823a8605edd
zzs <65422049@qq.com>
2019-05-05 09:51:42 +0800

    update

--- dubbo/boot-user-service-provider/src/main/java/com/zzs/gmall/impl/UserServiceImpl.java
+++ dubbo/boot-user-service-provider/src/main/java/com/zzs/gmall/impl/UserServiceImpl.java
@@ -12,7 +12,7 @@ public class UserServiceImpl implements UserService {
 
 
 	public List<UserAddress> getUserAddressList(String userId) {
-		System.out.println("UserServiceImpl...0...");
+		System.out.println("UserServiceImpl...20880...");
 		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
 		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
 //		if (Math.random()>0.5){
